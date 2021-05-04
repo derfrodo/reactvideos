5dc3c79712645d0d1171c6d1e05a03b5437a25bb
Stefan Pauka <paukastefan@gmail.com>
2021-05-04 08:10:00 +0200

    Add Demo React App

---
+++ React Hooks UseEffect/demo/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { EffectHook } from './EffectHook';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Wir lernen React
+        </h1>
+      </header>
+      <main>
+        <EffectHook />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ React Hooks UseEffect/demo/src/EffectHook.tsx
@@ -0,0 +1,3 @@
+export const EffectHook: React.FC<{}> = () => {
+  return <></>;
+};
