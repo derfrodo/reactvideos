a9c3274a2a85b982e64033a6d094c3ebec44657d
Stefan Pauka <paukastefan@gmail.com>
2021-05-08 18:08:54 +0200

    After video (.1) ;)

--- 1 - React Hooks UseState/demo/src/StateHook.tsx
+++ 1 - React Hooks UseState/demo/src/StateHook.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
 export const StateHook: React.FC = () => {
+  // let waveCount = 10;
   const [waveCount, setWaveCount] = useState<number>(0);
   return (
     <>
-      <p>Hallo lieber Zuschauer</p>
+      <p>Hallo lieber Zuschauer 👋</p>
       <div>
         <button
           onClick={() => {
-            setWaveCount((previous) => previous + 1);
-            console.log(waveCount);
+            // waveCount = waveCount + 1;
+            setWaveCount(previous => previous + 1);
+            setWaveCount(previous => previous + 1);
+            console.log({ waveCount });
           }}
         >
           👋
